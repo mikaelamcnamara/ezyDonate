7b19683fde9caa5958290f164fe2d2241e64aff9
Wasif <wasif.hossain@hotmail.com>
2019-04-15 11:12:23 +1000

    Finished Login Page

--- app/src/main/java/com/example/ezydonate/MainActivity.java
+++ app/src/main/java/com/example/ezydonate/MainActivity.java
@@ -1,37 +1,78 @@
 package com.example.ezydonate;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView search_bar; // Defining ListView
+    ArrayAdapter<String> adapter; // Defining ArrayAdapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /*Attempt in creating an arraylist for the search
+        search_bar = (ListView) findViewById(R.id.search_bar);
+        ArrayList<String> arraySearch = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.activity_list_item,
+                arraySearch
+        );
         // Toolbar toolbar = findViewById(R.id.toolbar);
         //   setSupportActionBar(toolbar);
-
+*/
 
     }
 
-    public void login(View view) {
 
+
+    public void login(View view) {
         setContentView(R.layout.content_main);
     }
 
+/* This is part of the search function
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        final MenuItem item = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+              adapter.getFilter().filter(s);;
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
+
+*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
