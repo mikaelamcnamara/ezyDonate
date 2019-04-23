4279dbc0497f454c3d1387b63f907843a8c638a0
Wasif <wasif.hossain@hotmail.com>
2019-04-23 21:56:54 +1000

    updated main page

--- app/src/main/java/com/example/ezydonate/MainActivity.java
+++ app/src/main/java/com/example/ezydonate/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.ezydonate;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -13,23 +23,35 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton logButton;
     ListView search_bar; // Defining ListView
     ArrayAdapter<String> adapter; // Defining ArrayAdapter
+    static final int requestcode = 1; //RequestCode
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
+
+
+
+    }
+
 
 
         /*Attempt in creating an arraylist for the search
         search_bar = (ListView) findViewById(R.id.search_bar);
         ArrayList<String> arraySearch = new ArrayList<>();
+
+
 
         adapter = new ArrayAdapter<String>(
                 MainActivity.this,
@@ -38,15 +60,29 @@ public class MainActivity extends AppCompatActivity {
         );
         // Toolbar toolbar = findViewById(R.id.toolbar);
         //   setSupportActionBar(toolbar);
+
+
+           androidImageButton = (ImageButton) findViewById(R.id.LogOutButton);
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+
+        });
 */
 
-    }
+
 
 
 
     public void login(View view) {
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.page_main);
     }
+    public void register(View view) {
+        setContentView(R.layout.page_main);
+    }
+
+
 
 /* This is part of the search function
     @Override
@@ -90,6 +126,57 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }*/
         return false;
+
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //PERMISSION GRANTED
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == MainActivity.requestcode) {
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+        } else {
+            showDetails();
+
+        }
+    }
+
+
+    public void showDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Storage  Write Permission")
+                .setMessage("This permission is necessary to access videos")
+                .setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        }
+
+
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        builder.create();
+        builder.show();
 
     }
 }
