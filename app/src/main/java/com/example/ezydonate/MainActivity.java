5226c43d764b58b1db134b7ded7ad9f59708793f
NadavF1 <49294818+NadavF1@users.noreply.github.com>
2019-04-26 00:32:58 +1000

    Allows for Name and Username to be uploaded.

--- app/src/main/java/com/example/ezydonate/MainActivity.java
+++ app/src/main/java/com/example/ezydonate/MainActivity.java
@@ -1,58 +1,37 @@
 package com.example.ezydonate;
-
 import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout dmLayout;
+
     Button b1;
-    ImageButton logButton;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
-    ListView search_bar; // Defining ListView
-    ArrayAdapter<String> adapter; // Defining ArrayAdapter;
-    static final int requestcode = 1; //RequestCode
     private FirebaseAuth mAuth;
+    static final int requestcode = 1;
 
     //lmaoooooo
     @Override
@@ -60,111 +39,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
-        checkPermission();
+        // Toolbar toolbar = findViewById(R.id.toolbar);
+        //   setSupportActionBar(toolbar);
         b1 = (Button) findViewById(R.id.loginbtn);
-        // mDrawerLayout = (DrawerLayout) findViewById(R.id.draw_layout);
+        checkPermission();
 
     }
 
     @Override
     public void onStart() {
         super.onStart();
-       // CreateNewUser(mAuth, "Test@gmail.com", "Adminss" , "Test", "DASDEASDASD");
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
+    public void logon (View view) {
+        setContentView(R.layout.page_main);
+    }
 
-    public void logout(View view) {
+    public void forgotPass (View view) {
+        setContentView(R.layout.forgot_user_password);
+    }
 
-        setContentView(R.layout.activity_main);
+    public void register (View view) {
+        setContentView(R.layout.register_page);
     }
 
 
-        /*Attempt in creating an arraylist for the search
-        search_bar = (ListView) findViewById(R.id.search_bar);
-        ArrayList<String> arraySearch = new ArrayList<>();
 
+    public void menu (View view) {
+        dmLayout = (DrawerLayout) findViewById(R.id.draw_layout);
+        dmLayout.openDrawer(Gravity.LEFT);
+    }
 
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //PERMISSION GRANTED
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
 
-        adapter = new ArrayAdapter<String>(
-                MainActivity.this,
-                android.R.layout.activity_list_item,
-                arraySearch
-        );
-        // Toolbar toolbar = findViewById(R.id.toolbar);
-        //   setSupportActionBar(toolbar);
-
-
-           androidImageButton = (ImageButton) findViewById(R.id.LogOutButton);
-        androidImageButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.activity_main);
             }
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        });
-*/
 
 
     public void login(View view) {
+        // R means res, layout is the package under res //
 
         EditText username = (EditText) findViewById(R.id.editText3);
         EditText password = (EditText) findViewById(R.id.editText);
-
         int i = 0;
         i++;
 
         /*
         if (username.getText().toString().equals("jshkeeg@gmail.com") && password.getText().toString().equals("adminss")) {
+
             CreateNewUser(mAuth, username.getText().toString(), password.getText().toString());
             setContentView(R.layout.content_main);
         } else if (i == 3) {
+
             b1.setEnabled(false);
         }
         */
         signIn(mAuth, username.getText().toString(),password.getText().toString());
+
+
     }
 
-    public void register(View view) {
-        setContentView(R.layout.page_main);
-    }
+    public void logout(View view) { setContentView(R.layout.activity_main); }
 
-
-    /* public void menu (View view) {
-        mDrawerLayout=(DrawerLayout) findViewById(R.id.draw_layout);
-        mDrawerLayout.openDrawer(Gravity.LEFT);
-        /* mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.Open, R.string.Close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();*/
-    /*}*/
-
-
-
-/* This is part of the search function
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        final MenuItem item = menu.findItem(R.id.search_bar);
-        SearchView searchView = (SearchView) item.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-              adapter.getFilter().filter(s);;
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
-
-*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -185,111 +138,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-/*
-
-   public boolean onNavigationItemSelected(MenuItem item) {
-       int id = item.getItemId();
-
-       if (id == R.id.nav_camera) {
-
-       } else if (id == R.id.nav_gallery) {
-
-       }
-       return true;
-   }
-*/
-
-
-    public Boolean CreateNewUser(final FirebaseAuth mAuth, String email, String password , final String name, final String username) {
-
-
-        if (email.trim().equals("") || password.trim().equals("")) {
-            Toast.makeText(null, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        Toast.makeText(MainActivity.this, "Creating...", Toast.LENGTH_SHORT).show();
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Successful Creation", Toast.LENGTH_SHORT).show();
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    //Change Directory Later
-                    DatabaseReference ref = database.getReference("users/Full_Name");
-                    ref.setValue(name);
-                    //Change Directory Later
-                    ref = database.getReference("users/Username");
-                    ref.setValue(username);
-                } else {
-                    Toast.makeText(MainActivity.this, "An error has occurred", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-        return true;
-    }
-
-    public void signIn(final FirebaseAuth mAuth, String email, String password) {
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    //@Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "signInWithEmail:success", Toast.LENGTH_SHORT).show();
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            setContentView(R.layout.page_main);
-
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
-
-    public void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                //PERMISSION GRANTED
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
-        }
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == MainActivity.requestcode) {
             Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
-
         } else {
             showDetails();
 
         }
     }
 
-
     public void showDetails() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Storage  Write Permission")
-                .setMessage("This permission is necessary to access videos")
+                .setMessage("This permission is necessary to access storage to be able to save data")
                 .setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                         }
-
-
                     }
                 })
                 .setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
@@ -301,6 +171,57 @@ public class MainActivity extends AppCompatActivity {
         builder.create();
         builder.show();
 
+    }
+
+
+
+    public Boolean CreateNewUser(final FirebaseAuth mAuth, String email, final String password, final String fullName, final String username) {
+        if (email.trim().equals("") || password.trim().equals("")) {
+            Toast.makeText(null, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        Toast.makeText(MainActivity.this, "Creating...", Toast.LENGTH_SHORT).show();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Successful Creation", Toast.LENGTH_SHORT).show();
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    //Change later
+                    DatabaseReference ref = database.getReference("user/username");
+                    ref.setValue(username);
+                    // Change later
+                    ref = database.getReference("user/password");
+                    ref.setValue(password);
+                } else {
+                    Toast.makeText(MainActivity.this, "An error has occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return true;
+    }
+
+
+
+
+
+    public void signIn(final FirebaseAuth mAuth, String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    //@Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "signInWithEmail:success", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            setContentView(R.layout.activity_main);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
     }
 
 }
