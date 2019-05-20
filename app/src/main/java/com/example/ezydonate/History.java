511d2d6bf3a6984a3dbc0678fe58f510723d2f4f
Mikaela McNamara <>
2019-05-20 13:34:33 +1000

    History

---
+++ app/src/main/java/com/example/ezydonate/History.java
@@ -0,0 +1,75 @@
+package com.example.ezydonate;
+
+/*Created a separate class for events */
+
+public class History {
+
+
+    public String title;
+    public String description;
+    public String location;
+    public String eventDate;
+    public String time;
+    public int image;
+
+    public History (String title, String description, String location, String eventDate, String time, int image) {
+
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.eventDate = eventDate;
+        this.time = time;
+        this.image = image;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getThumbnail() {
+        return image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
+
--- app/src/main/java/com/example/ezydonate/HistoryFragment.java
+++ app/src/main/java/com/example/ezydonate/HistoryFragment.java
@@ -3,14 +3,39 @@ package com.example.ezydonate;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends Fragment {
+    View v;
+    private RecyclerView myrecyclerview;
+    private List<Event> lstEvent;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        v = inflater.inflate(R.layout.fragment_event, container, false);
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.recyclerview_id);
+        RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(),lstEvent);
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myrecyclerview.setAdapter(recyclerAdapter);
+        return v;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        lstEvent = new ArrayList<>();
+        lstEvent.add(new Event("The Vegetarian", "yo", "hello","fdksdfk","ninehirty",R.drawable.app_icon));
+        lstEvent.add(new Event("The Vegetarianss", "yo", "hello","fdksdfk","ninehirty",R.drawable.app_icon));
+        lstEvent.add(new Event("The Vegetarian", "yo", "hello","fdksdfk","ninehirty",R.drawable.app_icon));
+        lstEvent.add(new Event("The Vegetarianss", "yo", "hello","fdksdfk","ninehirty",R.drawable.app_icon));
+    }
+
 }
