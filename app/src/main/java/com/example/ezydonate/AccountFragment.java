988f877225fa8c04f815d42bb6e801e63810d701
dennislomibao <MarlonDennisjr.B.Lomibao@student.uts.edu.au>
2019-05-20 17:28:54 +1000

    Account Mngment back-end

--- app/src/main/java/com/example/ezydonate/AccountFragment.java
+++ app/src/main/java/com/example/ezydonate/AccountFragment.java
@@ -1,16 +1,140 @@
 package com.example.ezydonate;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ezydonate.Model.UserInformation;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AccountFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+
+    private static final String TAG = "MainActivity";
+
+    /**
+     * Firebase stuff
+     */
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userID;
+    private UserInformation uInfo;
+
+
+    /**
+     *UI references
+     */
+    @BindView(R.id.tvFullname) TextView tvFullname;
+    @BindView(R.id.tvEmail) TextView tvEmail;
+    @BindView(R.id.tvPassword) TextView tvPassword;
+    @BindView(R.id.btnEdit) Button mBtnEdit;
+
+
+    public AccountFragment(){
+
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Note the use of getActivity() to reference the Activity holding this fragment
+        getActivity().setTitle("Account Information");
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        ButterKnife.bind(this,v);
+
+        return v;
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value + add exception
+                Log.w(TAG, "Failed to read value.");
+            }
+        });
+    }
+    /**
+     * Get data from firebase and store in local string
+     * @param dataSnapshot
+     */
+    private void showData(DataSnapshot dataSnapshot) {
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            uInfo = new UserInformation();
+            uInfo.setFullname(ds.child(userID).getValue(UserInformation.class).getFullname()); //set the name
+            uInfo.setEmail(ds.child(userID).getValue(UserInformation.class).getEmail()); //set the email
+            //uInfo.setPassword(ds.child(userID).getValue(UserInformation.class).getPassword()); //set the password
+
+
+            tvFullname.setText("Full Name: " + uInfo.getFullname());
+            tvEmail.setText("Email: " + uInfo.getEmail());
+            //tvPassword.setText("Password: " + uInfo.getPassword());
+
+
+        }
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(getActivity(),message,Toast.LENGTH_SHORT).show();
+    }
+
 }
